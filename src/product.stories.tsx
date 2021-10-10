dcddf140053f4d0dc2fa8235c6245b6b4c0d9340
Andrea Caldera <andrea.caldera@gmail.com>
2021-10-10 13:58:41 +0100

    cleanup

--- src/product.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Product } from "./product";
-import { aProduct } from "../__generated.dev__/generated-mocks";
-
-export default {
-  title: "Product",
-};
-
-export const product = () => <Product {...aProduct()} />;
