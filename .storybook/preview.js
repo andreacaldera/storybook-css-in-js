2367299bbb293d334dcef113effb41588921cbd6
Andrea Caldera <andrea.caldera@gmail.com>
2021-10-10 14:50:31 +0100

    remove emotion dep

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
-import { ThemeProvider } from 'emotion-theming';
-import { addDecorator } from '@storybook/react';
-import { withThemes } from '@react-theming/storybook-addon';
-import { theme, invertedTheme } from '../src/theme';
+import { addDecorator } from "@storybook/react";
+import { withThemes } from "@react-theming/storybook-addon";
+import { theme, invertedTheme } from "../src/theme";
+import { ThemeProvider } from "styled-components";
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
